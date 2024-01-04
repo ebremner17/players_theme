bd04ec2f6d6497bef11729922373ae61fd4a8250
Martin Leblanc <m26lebla@uwaterloo.ca>
2024-01-04 13:58:54 -0500

    martin-fix: fix the flex

--- js/games-owl.js
+++ js/games-owl.js
@@ -3,7 +3,7 @@
     attach: function (context, settings) {
       $(document).ready(function(){
 
-        $('.owl-carousel').owlCarousel({
+        $('.block-cbl-games-block .owl-carousel').owlCarousel({
           loop: false,
           nav: true,
           rewind: true,
@@ -14,16 +14,30 @@
             768:{
               items:2
             },
-                1024:{
-                    items:3
-                },
-                1296:{
-                    items:4
-                },
-                1600:{
-                    items:5
-                }
+            1024:{
+                items:3
+            },
+            1296:{
+                items:4
+            },
+            1600:{
+                items:5
             }
+          }
+        })
+
+        $('.players-tournaments-block .owl-carousel').owlCarousel({
+          loop: false,
+          nav: true,
+          rewind: true,
+          responsive:{
+            0:{
+              items:1
+            },
+            768:{
+              items:2
+            },
+          }
         })
       });
     }
